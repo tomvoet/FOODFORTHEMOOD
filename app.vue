b2190383c9e045a5727b13d82e6d3a6f6a4c73b8
Tom Voet <tomvoet@gmx.de>
2022-09-10 00:35:34 +0200

    init

---
+++ app.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <NuxtWelcome />
+  </div>
+</template>
+
+<script setup>
+await useAsyncData(async () => {
+  const response = await $fetch('/api/hello')
+  console.log(response)
+})
+</script>
---
+++ components/Navbar.vue
@@ -0,0 +1,9 @@
+<script lang="ts" setup></script>
+
+<template>
+  <div>
+    Component: Navbar
+  </div>
+</template>
+
+<style scoped></style>
