72518c5f21f253aff5bdb638c2996c80f4b504d0
Tom Voet <tomvoet@gmx.de>
2022-09-10 12:06:31 +0200

    added layout and page directory

---
+++ layouts/default.vue
@@ -0,0 +1,13 @@
+<script lang="ts" setup></script>
+
+<template>
+  <div id="app">
+    <Navbar />
+    Layout: default
+    <slot />
+  </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ layouts/test.vue
@@ -0,0 +1,12 @@
+<script lang="ts" setup></script>
+
+<template>
+    <div id="app">
+        Layout: test
+        <slot />
+    </div>
+</template>
+
+<style scoped>
+
+</style>
