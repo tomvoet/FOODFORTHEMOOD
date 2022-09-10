0bc79e1c55ed1aad6a4f19dfea8a37b4944bc4ce
Tom Voet <tomvoet@gmx.de>
2022-09-10 19:09:29 +0200

    First webpage endpoints

---
+++ pages/post/[id].vue
@@ -0,0 +1,27 @@
+<script lang="ts" setup>
+import { Post } from '.prisma/client';
+
+const route = useRoute()
+
+const { data } = await useAsyncData(() =>
+    $fetch<Post>(`/api/post/${route.params.id}`)
+)
+
+const post = data.value
+</script>
+    
+<template>
+    <div>
+        <div v-if="post">
+            {{ post }}
+        </div>
+        <div v-else>
+            Loading...
+        </div>
+    </div>
+</template>
+    
+<style scoped>
+dasd
+</style>
+    
---
+++ pages/user/[username].vue
@@ -0,0 +1,26 @@
+<script lang="ts" setup>
+import { User } from '.prisma/client';
+
+const route = useRoute()
+
+const { data } = await useAsyncData(() =>
+  $fetch<User>(`/api/user/${route.params.username}`)
+)
+
+const user = data.value
+</script>
+
+<template>
+  <div>
+    <div v-if="user">
+      {{ user }}
+    </div>
+    <div v-else>
+      Loading...
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
