a8b0d48ec76c222864d52d78ee1681f7f892a706
Tom Voet <tomvoet@gmx.de>
2022-09-10 13:07:09 +0200

    added title metadata

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,8 +1,27 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+const route = useRoute()
+
+onMounted(() => {
+  document.onscroll = () => {
+    const nav = document.getElementById("nav")
+    if (nav) {
+      if (window.scrollY > 0) {
+        nav.classList.add("scrolled")
+      } else {
+        nav.classList.remove("scrolled")
+      }
+    }
+  }
+})
+
+useHead({
+  title: `Testapp - ${route.meta.title}`,
+})
+</script>
 
 <template>
   <div id="app">
-    <Navbar />
+    <Navbar id="nav" />
     Layout: default
     <slot />
   </div>
