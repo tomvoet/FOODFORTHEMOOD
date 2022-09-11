3bbf64db5b7e6d2285f84b8320971f90ae1c8d14
Tom Voet <tomvoet@gmx.de>
2022-09-11 02:43:22 +0200

    basic routing

---
+++ pages/user/index.vue
@@ -0,0 +1,29 @@
+<script lang="ts" setup>
+import { User } from '.prisma/client';
+
+const { data } = await useFetch<{ status: number, users: User[] }>(`/api/user`)
+
+const status = data.value?.status
+const users = data.value?.users
+</script>
+    
+<template>
+    <div>
+        <div v-if="status == 200">
+            <div v-for="user of users">
+                <NuxtLink :to="'/user/' + user.username">{{user}}</NuxtLink>
+            </div>
+        </div>
+        <div v-else-if="status == 404">
+            404
+        </div>
+        <div v-else>
+            Loading...
+        </div>
+    </div>
+</template>
+    
+<style scoped>
+
+</style>
+    
