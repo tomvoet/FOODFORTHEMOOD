18e4ccf09f8e74e8c400a162f3aea4b5a3b288c2
Tom Voet <tomvoet@gmx.de>
2022-09-12 13:21:24 +0200

    fixed request types

--- server/api/post/index.ts
+++ server/api/post/index.ts
@@ -4,63 +4,29 @@ import { Post } from "@prisma/client"
 export default defineEventHandler(async (event) => {
     const query = useQuery(event)
 
-    if (query.author !== undefined && query.author !== null) {
-        const username: string = query.author.toString()
-
-        const postList = await prisma.user.findUnique({
-            where: {
-                username: username,
-            },
-            select: {
-                posts: {
-                    select: {
-                        id: true,
-                        authorId: true,
-                        title: true,
-                        content: true,
-                        createdAt: true,
-                        published: true,
-                    },
+    const posts = await prisma.post.findMany({
+        select: {
+            id: true,
+            authorId: true,
+            title: true,
+            content: true,
+            createdAt: true,
+            published: true,
+            author: {
+                select: {
+                    id: true,
+                    username: true,
                 },
             },
-        })
+        },
+    })
 
-        if (!postList) {
-            return {
-                status: 404,
-                posts: null,
-            }
+    if (!posts) {
+        return {
+            status: 404,
+            posts: null,
         }
-        const posts: Post[] = postList?.posts
-
-        return { status: 200, posts }
-    } else {
-        const posts = await prisma.post.findMany({
-            select: {
-                id: true,
-                authorId: true,
-                title: true,
-                content: true,
-                createdAt: true,
-                published: true,
-                author: {
-                    select: {
-                        id: true,
-                        username: true,
-                    },
-                },
-            },
-        })
-
-        if (!posts) {
-            return {
-                status: 404,
-                posts: null,
-            }
-        }
-
-        posts[0].author
-
-        return { status: 200, posts: posts }
     }
+
+    return { status: 200, posts: posts }
 })
---
+++ server/api/user/[username]/posts.ts
@@ -0,0 +1,34 @@
+import prisma from "@/server/services/dbManager"
+
+export default defineEventHandler(async (event) => {
+    const { username } = event.context.params
+
+    const postList = await prisma.user.findUnique({
+        where: {
+            username: username,
+        },
+        select: {
+            posts: {
+                select: {
+                    id: true,
+                    authorId: true,
+                    title: true,
+                    content: true,
+                    createdAt: true,
+                    published: true,
+                },
+            },
+        },
+    })
+
+    if (!postList) {
+        return {
+            status: 404,
+            posts: null,
+        }
+    }
+
+    const posts = postList?.posts
+
+    return { status: 200, posts }
+})
