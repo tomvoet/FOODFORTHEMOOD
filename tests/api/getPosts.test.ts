28360302f0ed893295b03fc07654ddf78dfc0207
Tom Voet <tomvoet@gmx.de>
2022-09-13 15:17:43 +0200

    added test cases

--- tests/api/getPosts.test.ts
+++ tests/api/getPosts.test.ts
@@ -1,14 +1,39 @@
-import { expect } from "chai"
-import { describe, it } from "vitest"
+import { expect, describe, it } from "vitest"
 import { $fetch, setup } from "@nuxt/test-utils-edge"
+import { Post } from "@prisma/client"
 
 await setup({
     server: true,
 })
 
 describe("getPosts - homePage", () => {
+    it("should return 200", async () => {
+        const response = await $fetch("/api/post")
+        expect(response.status).toBe(200)
+    })
     it("should return posts", async () => {
         const res = await $fetch("/api/post")
-        expect(res.status).to.equal(200)
+        expect(res.status).toEqual(200)
+        expect(Array.isArray(res.posts)).toBeTruthy()
+        expect(res.posts.length).toBeGreaterThan(0)
+    })
+    it("should return valid posts", async () => {
+        const res: { status: number; posts: Post[] } = await $fetch("/api/post")
+        expect(res.status).toEqual(200)
+        expect(Array.isArray(res.posts)).toBeTruthy()
+        expect(res.posts.length).toBeGreaterThan(0)
+        res.posts.forEach((post: Post) => {
+            expect(post.id).toBeTruthy()
+            expect(post.title).toBeTruthy()
+            expect(post.content).toBeTruthy()
+            expect(post.authorId).toBeTruthy()
+            expect(post.createdAt).toBeTruthy()
+        })
+    })
+    it("should return posts with pagination", async () => {
+        const res = await $fetch("/api/post?limit=2&offset=1")
+        expect(res.status).toEqual(200)
+        expect(Array.isArray(res.posts)).toBeTruthy()
+        expect(res.posts.length).toEqual(2)
     })
 })
