ecdbc799dfe48c3c49eeca4bc4b8da4f8546c283
Tom Voet <tomvoet@gmx.de>
2022-09-26 13:59:21 +0200

    added restaurant page

--- pages/restaurant/[id].vue
+++ pages/restaurant/[id].vue
@@ -1,8 +1,21 @@
 <script setup lang="ts">
+const route = useRoute()
 
+const { data } = await useFetch(`/api/restaurant/${route.params.id}`)
+
+const restaurant = data.value?.restaurant
+
+const { data: reviews } = await useFetch(`/api/restaurant/${route.params.id}/posts`)
+
+setMetadata(restaurant?.name ? restaurant.name : "404", `Reviews of ${restaurant?.name ? restaurant.name : "404"}`)
 </script>
 
-<template></template>
+<template>
+    <div>
+        <h1>{{ restaurant?.name }}</h1>
+        <p>{{ restaurant?.cuisines }}</p>
+    </div>
+</template>
 
 <style scoped>
 
---
+++ server/api/restaurant/[id].get.ts
@@ -0,0 +1,35 @@
+import prisma from "@/server/services/dbManager"
+
+export default defineEventHandler(async (event) => {
+    const { id } = event.context.params
+
+    const restaurant = await prisma.restaurant.findUnique({
+        where: {
+            id: Number(id),
+        },
+        select: {
+            id: true,
+            name: true,
+            state: true,
+            city: true,
+            cuisines: {
+                select: {
+                    id: true,
+                    name: true,
+                }
+            },
+        }
+    })
+
+    if (!restaurant) {
+        return {
+            status: 404,
+            restaurant: null,
+        }
+    }
+
+    return {
+        status: 200,
+        restaurant,
+    }
+})
