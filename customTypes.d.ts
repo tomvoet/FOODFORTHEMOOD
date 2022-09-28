382097ade720775d88266ad79a068ee6b64615a4
Tom Voet <tomvoet@gmx.de>
2022-09-28 12:51:25 +0200

    Further typing

--- customTypes.d.ts
+++ customTypes.d.ts
@@ -1,7 +1,14 @@
 import { Prisma } from '@prisma/client'
 
+type Omit<T, K extends keyof T> = Pick<T, Exclude<keyof T, K>>
+type PartialBy<T, K extends keyof T> = Omit<T, K> & Partial<Pick<T, K>>
+
 type FullPost = Prisma.PostGetPayload<{
   include: {
-    restaurant: true,
-    author: true
-  }}>
+    restaurant: true
+  }
+}>
+
+type singleLike =  {
+  username: string
+}
