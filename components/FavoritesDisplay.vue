69ecf92e22df46b44ad83a8117ddcabfa7aabad0
Tom Voet <tomvoet@gmx.de>
2022-09-28 12:52:01 +0200

    Added FavoritesDisplay Component to Post

---
+++ components/FavoritesDisplay.vue
@@ -0,0 +1,14 @@
+<script lang="ts" setup>
+import { singleLike } from '~~/customTypes';
+
+const props = defineProps<{
+    favorites?: singleLike[]
+}>()
+
+const isLiked = (favorites: singleLike[] | undefined) => favorites?.some(favorite => favorite.username == 'tomvoet')
+
+</script>
+
+<template>
+    <span :style="{ color: isLiked(favorites) ? 'red' : 'black' }">♥{{ favorites ? favorites.length : ""}}</span>
+</template>
