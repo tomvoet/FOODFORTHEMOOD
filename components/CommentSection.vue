1c26e88f8e97314cc26edf1038d55691bf5beb99
Tom Voet <tomvoet@gmx.de>
2022-09-28 12:51:48 +0200

    Added CommentSection Component to Post

---
+++ components/CommentSection.vue
@@ -0,0 +1,17 @@
+<script lang="ts" setup>
+import { Comment } from "@prisma/client"
+import { PartialBy } from "~~/customTypes";
+
+const props = defineProps<{
+    comments?: PartialBy<Comment, "postId">[]
+}>()
+</script>
+
+<template>
+    <div v-if="comments" v-for="comment in comments" :key="comment.id">
+        {{comment.text}}
+    </div>
+    <div v-else>
+        No comments yet
+    </div>
+</template>
