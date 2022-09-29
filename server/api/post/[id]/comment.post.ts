685443406bbc95adba789a5332224f81de1acd45
Tom Voet <tomvoet@gmx.de>
2022-09-29 12:12:20 +0200

    added new comment endpoint

---
+++ server/api/post/[id]/comment.post.ts
@@ -0,0 +1,24 @@
+import { prisma } from "@/server/services/dbManager"
+
+export default defineEventHandler(async (event) => {
+    const body = await useBody(event)
+    const { user, authorId, postId, text } = body
+
+    console.log(user)
+
+    if(!authorId && !postId && !text) return { status: 400, comment: null }
+
+    if(authorId !== user.username) return { status: 401, comment: null } //prevent commenting under wrong username
+
+    const comment = await prisma.comment.create({
+        data: {
+            authorId,
+            postId,
+            text,
+        }
+    })
+
+    if(!comment) return { status: 400, comment: null }
+
+    return { status: 200, comment }
+})
