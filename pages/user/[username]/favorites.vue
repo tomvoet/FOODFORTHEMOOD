09afba9146e87b03a8b3b461fc945dcd17018116
Tom Voet <tomvoet@gmx.de>
2022-09-30 17:05:45 +0200

    favorites page for user

--- pages/user/[username]/favorites.vue
+++ pages/user/[username]/favorites.vue
@@ -1,3 +1,41 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+const route = useRoute()
 
-<template>Favorites</template>
+const username = computed(() => route.params.username as string)
+
+const { data } = useFetch(`/api/user/${username.value}/favorites`, {
+    key: `user/${username.value}/favorites`,
+    server: false,
+})
+
+const favorites = computed(() => data.value?.favorites)
+const favoritesStatus = computed(() => data.value?.status)
+</script>
+
+<template>
+    <div v-if="favoritesStatus == 200" class="postsContainer">
+        <PostComp
+            v-for="post in favorites"
+            :key="post.id"
+            :post="post"
+            :author="{ username: username }"
+            :restaurant="{ ...post.restaurant, id: post.restaurantId }"
+            :favorites="post.favorites"
+            :comments="post.comments"
+        />
+    </div>
+    <div v-else-if="favoritesStatus == 404">
+        <h1>No posts found</h1>
+    </div>
+    <div v-else>
+        <h1>Loading...</h1>
+    </div>
+</template>
+
+<style scoped>
+.postsContainer {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+}
+</style>
