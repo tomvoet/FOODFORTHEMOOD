f2f5835baf3ef99e4a448f56a0e57ca1f4fa6c6c
evabecks <[eva.becker02@gmail.com]>
2022-10-05 21:36:05 +0200

    register fix

--- pages/register.vue
+++ pages/register.vue
@@ -23,39 +23,64 @@ const register = async () => {
 }
 
 const validate = () => {
+    var check = 0;
+
+    var emailCheck = validateEmail()
+
+    var passwordCheck = validatePassword()
+
     if (password.value !== passwordConfirmation.value) {
-        console.log("Passwords do not match")
-    } else {
+        document.getElementById("passwordConfCheck")!.innerText = "Passwords do not match";
+        document.getElementById("passwordConfCheck")!.style.display = "inline";
+        check = 1;
+    }
+    if (!emailCheck){
+        document.getElementById("emailCheck")!.innerText = "Invalid email";
+        document.getElementById("emailCheck")!.style.display = "inline";
+        check = 1;
+    }
+    if(!passwordCheck) {
+        document.getElementById("passwordCheck")!.innerText = "Password must be at least 8 characters, contain multiple uppercase and lowercase letters, multiple digits and a special case letter (!@#$&*)";
+        document.getElementById("passwordCheck")!.style.display = "inline";
+        check = 1;
+    }
+    if(username.value.length < 3) {
+        document.getElementById("usernameCheck")!.innerText = "Username must be at least 3 characters";
+        document.getElementById("usernameCheck")!.style.display = "inline";
+        check = 1;
+    }
+    if(check == 0) {
         register()
-    }
+    } 
 }
 
-const validateEmail = () => {
-    if (!email.value.includes("@")) {
-        console.log("Invalid email")
-    } else {
-        validate()
-    }
+function validateEmail(): boolean {
+    const regexp = new RegExp(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
+
+    return regexp.test(email.value)
 }
 
-const validatePassword = () => {
-    if (password.value.length < 8) {
-        console.log("Password must be at least 8 characters")
-    } else {
-        validateEmail()
-    }
+function validatePassword(): boolean {
+    const regexp = new RegExp(/^(?=.*[A-Z].*[A-Z])(?=.*[!@#$&*])(?=.*[0-9].*[0-9])(?=.*[a-z].*[a-z].*[a-z]).{8}$/);
+
+    return regexp.test(password.value)
 }
 
-const validateUsername = () => {
-    if (username.value.length < 3) {
-        console.log("Username must be at least 3 characters")
-    } else {
-        validatePassword()
-    }
-}
+/* explanation of used regex in validatePassword(), if changes are supposed to be made
+^                         Start anchor
+(?=.*[A-Z].*[A-Z])        Ensure string has two uppercase letters.
+(?=.*[!@#$&*])            Ensure string has one special case letter.
+(?=.*[0-9].*[0-9])        Ensure string has two digits.
+(?=.*[a-z].*[a-z].*[a-z]) Ensure string has three lowercase letters.
+.{8}                      Ensure string is of length 8.
+$                         End anchor. */
 
 const submit = () => {
-    validateUsername()
+    document.getElementById("usernameCheck")!.style.display = "none";
+    document.getElementById("passwordCheck")!.style.display = "none";
+    document.getElementById("passwordConfCheck")!.style.display = "none";
+    document.getElementById("emailCheck")!.style.display = "none";
+    validate()
 }
 
 setMetadata("Register", "Register for an account.")
@@ -65,18 +90,22 @@ setMetadata("Register", "Register for an account.")
     <form class="registerContainer">
         <h1>Register</h1>
         <div>
+            <p id="usernameCheck"></p>
             <label for="username">Username</label>
             <input id="username" v-model="username" type="text" />
         </div>
         <div>
+            <p id="emailCheck"></p>
             <label for="email">Email</label>
-            <input id="email" v-model="email" type="text" />
+            <input id="email" v-model="email" type="email" />
         </div>
         <div>
+            <p id="passwordCheck"></p>
             <label for="password">Password</label>
             <input id="password" v-model="password" type="password" />
         </div>
         <div>
+            <p id="passwordConfCheck"></p>
             <label for="passwordConfirmation">Confirm Password</label>
             <input
                 id="passwordConfirmation"
