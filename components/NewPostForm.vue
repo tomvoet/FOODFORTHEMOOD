b7db1771d368f0df3e271ec08bc95d1a58209623
Tom Voet <tomvoet@gmx.de>
2022-10-05 19:27:02 +0200

    rough structure of new post form

--- components/NewPostForm.vue
+++ components/NewPostForm.vue
@@ -1,9 +1,14 @@
 <script lang="ts" setup>
 const title = ref("")
 const content = ref("")
+const rating = ref(0)
 
 const createPost = () => {
-    console.log(title.value, content.value)
+    console.log(title.value, content.value, rating.value)
+}
+
+const updateRating = (newRating: number) => {
+    rating.value = newRating
 }
 </script>
 
@@ -13,13 +18,29 @@ const createPost = () => {
         <form @submit.prevent="createPost">
             <div>
                 <label for="title">Title</label>
-                <input id="title" v-model="title" type="text" />
+                <input
+                    id="title"
+                    v-model="title"
+                    class="border-gray-300 outline-1"
+                    type="text"
+                />
             </div>
             <div>
                 <label for="content">Content</label>
-                <textarea id="content" v-model="content" />
+                <textarea
+                    id="content"
+                    v-model="content"
+                    class="border-gray-300 outline-1"
+                ></textarea>
             </div>
-            <button type="submit">Submit</button>
+            <div>
+                <label for="rating">Rating</label>
+                <RatingSelector
+                    :rating="rating"
+                    @update-rating="updateRating"
+                />
+            </div>
+            <button type="submit">Create Post</button>
         </form>
     </div>
 </template>
