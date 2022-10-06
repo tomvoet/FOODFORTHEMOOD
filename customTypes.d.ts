f5f3d7dc7b2eb83ffe5c001489a6962eb9c941cc
Tom Voet <tomvoet@gmx.de>
2022-10-06 18:19:31 +0200

    new custom type for fullpost

--- customTypes.d.ts
+++ customTypes.d.ts
@@ -1,14 +1,21 @@
-import { Prisma } from "@prisma/client"
+import { Post, Comment } from "@prisma/client"
 
 type Omit<T, K extends keyof T> = Pick<T, Exclude<keyof T, K>>
 type PartialBy<T, K extends keyof T> = Omit<T, K> & Partial<Pick<T, K>>
 
-type FullPost = Prisma.PostGetPayload<{
-    include: {
-        restaurant: true
-    }
-}>
-
 type singleLike = {
     username: string
+}
+
+interface FullPost extends Post {
+    favorites: singleLike[]
+    comments: {
+        id: number
+        authorId: string
+        createdAt: Date
+        text: string
+    }[]
+    restaurant: {
+        name: string
+    }
 }
