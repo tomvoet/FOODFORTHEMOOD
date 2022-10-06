26fe2348319dc76e41e923b615add7c6c1f6b3c1
Tom Voet <tomvoet@gmx.de>
2022-10-06 20:24:57 +0200

    basic new Post component

--- components/NewPostForm.vue
+++ components/NewPostForm.vue
@@ -1,46 +1,105 @@
 <script lang="ts" setup>
+import type { minimalRestaurant } from "@/customTypes"
+
 const title = ref("")
-const content = ref("")
+const text = ref("")
 const rating = ref(0)
+const restaurantChoice = ref(0)
+const chosenFood = ref("")
+
+const restaurants = ref([] as minimalRestaurant[] | undefined | null)
+const restaurantsStatus = ref(0)
 
 const createPost = () => {
-    console.log(title.value, content.value, rating.value)
+    console.log(
+        title.value,
+        text.value,
+        rating.value,
+        restaurantChoice.value,
+        chosenFood.value
+    )
 }
 
 const updateRating = (newRating: number) => {
     rating.value = newRating
 }
+
+const { data: restaurantData } = useFetch(`/api/restaurant`, {
+    key: `/api/restaurant`,
+    server: false,
+})
+
+if (restaurantData) {
+    restaurants.value = restaurantData.value?.restaurants
+    restaurantsStatus.value = restaurantData.value?.status || 0
+}
+
+watch(restaurantData, (data) => {
+    restaurants.value = data?.restaurants
+    restaurantsStatus.value = data?.status || 0
+})
 </script>
 
 <template>
     <div>
-        <h1>New Post</h1>
+        <h1 class="text-2xl font-bold">New Post</h1>
         <form @submit.prevent="createPost">
-            <div>
-                <label for="title">Title</label>
+            <div class="p-3">
+                <label for="title" class="block">Title</label>
                 <input
                     id="title"
                     v-model="title"
-                    class="border-gray-300 outline-1"
+                    class="border rounded-md p-1 focus:outline-none focus:ring-2 focus:ring-blue-500"
                     type="text"
                 />
             </div>
-            <div>
-                <label for="content">Content</label>
-                <textarea
-                    id="content"
-                    v-model="content"
-                    class="border-gray-300 outline-1"
-                ></textarea>
+            <div class="p-3">
+                <label for="restaurant" class="block">Restaurant</label>
+                <select
+                    id="restaurant"
+                    v-model="restaurantChoice"
+                    class="border rounded-md p-1 focus:outline-none focus:ring-2 focus:ring-blue-500"
+                >
+                    <option selected value="0">Select a restaurant</option>
+                    <option
+                        v-for="restaurant in restaurants"
+                        :key="restaurant.id"
+                        :value="restaurant.id"
+                    >
+                        {{ restaurant.name }}
+                    </option>
+                </select>
             </div>
-            <div>
-                <label for="rating">Rating</label>
+            <div class="p-3">
+                <label for="rating" class="block">Rating</label>
                 <RatingSelector
                     :rating="rating"
                     @update-rating="updateRating"
                 />
             </div>
-            <button type="submit">Create Post</button>
+            <div class="p-3">
+                <label for="text" class="block resizable-y">Content</label>
+                <textarea
+                    id="text"
+                    v-model="text"
+                    class="border rounded-md p-1 focus:outline-none focus:ring-2 focus:ring-blue-500"
+                ></textarea>
+            </div>
+            <div class="p-3">
+                <label for="chosenFood" class="block">Chosen Food</label>
+                <input
+                    id="chosenFood"
+                    v-model="chosenFood"
+                    class="border rounded-md p-1 focus:outline-none focus:ring-2 focus:ring-blue-500"
+                    type="text"
+                />
+            </div>
+            <button
+                type="submit"
+                class="border p-1 rounded-[.35em] hover:bg-blue-500 hover:text-white"
+            >
+                Create Post
+            </button>
         </form>
     </div>
 </template>
