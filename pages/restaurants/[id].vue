304f5ab43caa97bafb9d66b45c4b31e1034be523
Tom Voet <tomvoet@gmx.de>
2022-10-06 18:44:38 +0200

    fixed restaurant fetch cache issues

--- pages/restaurants/[id].vue
+++ pages/restaurants/[id].vue
@@ -3,19 +3,27 @@ import type { FullPost } from "@/customTypes"
 
 const route = useRoute()
 
-const { data } = await useFetch(`/api/restaurant/${route.params.id}`)
+const { data } = await useFetch(`/api/restaurant/${route.params.id}`, {
+    key: `restaurant/${route.params.id}`,
+    server: true,
+})
 
 const restaurant = data.value?.restaurant
+const restaurantStatus = data.value?.status || 0
 
 const reviews = ref([] as FullPost[] | undefined | null)
 const reviewsStatus = ref(0)
 
 const { data: reviewsData } = useFetch(
-    `/api/restaurant/${route.params.id}/posts`
+    `/api/restaurant/${route.params.id}/posts`,
+    {
+        key: `restaurant/${route.params.id}/posts`,
+        server: false,
+    }
 )
 
 if (reviewsData) {
-    reviews.value = reviewsData.value?.favorites
+    reviews.value = reviewsData.value?.posts
     reviewsStatus.value = reviewsData.value?.status || 0
 }
 
@@ -30,7 +38,7 @@ setMetadata(
 </script>
 
 <template>
-    <div>
+    <div v-if="restaurantStatus == 200 && restaurant">
         <h1>{{ restaurant?.name }}</h1>
         <p>{{ restaurant?.cuisines }}</p>
         <div v-if="reviewsStatus === 200" class="postsContainer">
@@ -44,6 +52,7 @@ setMetadata(
                 :comments="review.comments"
             />
         </div>
+        <StatusComp v-else :status="reviewsStatus" />
     </div>
 </template>
 
