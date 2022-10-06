7692ef42d5fd5816ec984b08f384bc584b2043df
Tom Voet <tomvoet@gmx.de>
2022-10-06 18:22:19 +0200

    removed unnecessary import

--- customTypes.d.ts
+++ customTypes.d.ts
@@ -1,4 +1,4 @@
-import { Post, Comment } from "@prisma/client"
+import { Post } from "@prisma/client"
 
 type Omit<T, K extends keyof T> = Pick<T, Exclude<keyof T, K>>
 type PartialBy<T, K extends keyof T> = Omit<T, K> & Partial<Pick<T, K>>
