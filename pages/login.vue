5dbf6c6f6ac19204c4440371bee03a75a178c85c
Tom Voet <tomvoet@gmx.de>
2022-10-12 22:17:43 +0200

    login hilfe :)

--- pages/login.vue
+++ pages/login.vue
@@ -9,8 +9,6 @@ const emailInput = ref("")
 
 const errorMessage = ref(false)
 
-const modalOpen = ref(false)
-
 const login = async () => {
     const loginRes = await userStore.login(username.value, password.value)
     if (loginRes.success) {
@@ -28,14 +26,6 @@ onMounted(() => {
         document.getElementById("username")?.focus()
     }, 100)
 })
-
-const openModal = () => {
-    modalOpen.value = true
-}
-
-const closeModal = () => {
-    modalOpen.value = false
-}
 
 //email checken, obs ne email ist, request an backend schicken
 const sendMail = () => {
@@ -81,28 +71,13 @@ setMetadata("Login", "Login to your account.")
         >
             Login
         </button>
-        <button
-            type="submit"
-            class="p-1 float-right hover:underline"
-            @click.prevent="openModal"
-        >
-            Forgot Password?
-        </button>
     </form>
-    <ModalComp>
-        <div
-            v-if="modalOpen"
-        class="fixed top-0 left-0 w-screen h-screen bg-black bg-opacity-50 flex flex-col justify-center items-center z-50"
-        @click.self="closeModal"
+    <ModalComp
+        text="Forgot Password?"
+        classes="p-1 float-right hover:underline"
     >
-        <aside class="bg-white rounded-lg p-6 relative">
-            <button
-                class="absolute top-3 right-3 leading-3 h-6 w-6 text-2xl text-gray-500 hover:text-gray-700 hover:bg-gray-200 rounded-md"
-                @click="closeModal()"
-            >
-                &times;
-            </button>
-            <p class="test">
+        <aside class="p-6 relative text-center">
+            <p>
                 Did you forget your password?<br />Please enter your E-Mail
                 address below.<br />We will send you an email to reset your
                 password.
@@ -110,22 +85,20 @@ setMetadata("Login", "Login to your account.")
             <div class="p-3">
                 <label for="email" class="block">Email</label>
                 <input
-                v-model="emailInput"
                     id="emailInput"
+                    v-model="emailInput"
                     class="border rounded-md p-1 focus:outline-none focus:ring-2 focus:ring-blue-500"
                     type="email"
                 />
             </div>
             <button
-                class="absolute leading-5 h-12 w-24 text-l text-black-500 hover:underline hover:bg-gray-100 rounded-md shadow"
+                class="mx-auto leading-5 h-12 w-24 text-l text-black-500 bg-gray-300 bg-opacity-40 hover:underline hover:bg-gray-100 rounded-md shadow"
                 @click="sendMail()"
             >
                 Send Mail
             </button>
-            <slot></slot>
         </aside>
-    </div></ModalComp>
-    
+    </ModalComp>
 </template>
 
 <style scoped></style>
