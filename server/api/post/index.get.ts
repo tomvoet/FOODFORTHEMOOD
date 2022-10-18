f4c71c9c2e06be68f7df8f16f73c41bc9265f48c
Tom Voet <tomvoet@gmx.de>
2022-10-18 23:39:45 +0200

    added cursor pagination to all posts

--- server/api/post/index.get.ts
+++ server/api/post/index.get.ts
@@ -5,10 +5,23 @@ export default defineEventHandler(async (event) => {
 
     const limit = query.limit
     const offset = query.offset
+    const cursor = query.cursorId
 
-    const params = {
+    const params: {
+        take: number
+        skip: number
+        cursor?: {
+            id: number
+        }
+    } = {
         take: Number(limit) || 10,
-        skip: Number(offset) || 0,
+        skip: !Number(cursor) ? Number(offset) || 0 : 1,
+    }
+
+    if (Number(cursor)) {
+        params.cursor = {
+            id: Number(cursor),
+        }
     }
 
     const posts = await prisma.post.findMany({
