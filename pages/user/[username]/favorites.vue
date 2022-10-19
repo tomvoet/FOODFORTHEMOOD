ecda359ff572a75b7fc37b3b61aec1c7e038ec0f
Tom Voet <tomvoet@gmx.de>
2022-10-19 19:12:20 +0200

    added infinite scroll to favorites page

--- pages/user/[username]/favorites.vue
+++ pages/user/[username]/favorites.vue
@@ -5,29 +5,69 @@ const route = useRoute()
 
 const username = computed(() => route.params.username as string)
 
-const favorites = ref([] as PartialBy<FullPost, "author">[] | undefined | null)
+const cursorObj = ref({} as { cursor: number })
+const endOfFeed = ref(false)
+
+const favorites = ref([] as PartialBy<FullPost, "author">[])
 const favoritesStatus = ref(0)
 
 const { data: favData, pending } = useLazyFetch(
-    `/api/user/${username.value}/favorites`,
+    `/api/user/${username.value}/favorites${
+        cursorObj.value.cursor ? `?cursor=${cursorObj.value.cursor}` : ""
+    }`,
     {
-        key: `user/${username.value}/favorites`,
+        key: `user/${username.value}/favorites${
+            cursorObj.value.cursor ? `?cursor=${cursorObj.value.cursor}` : ""
+        }`,
     }
 )
 
-if (favData) {
+if (favData && favData.value?.favorites) {
     favorites.value = favData.value?.favorites
     favoritesStatus.value = favData.value?.status || 0
 }
 
 watch(favData, (data) => {
     favorites.value = data?.favorites
+        ? [...favorites.value, ...data.favorites]
+        : []
     favoritesStatus.value = data?.status || 0
 })
 
 const reloadFavorites = () => {
     refreshNuxtData()
 }
+
+const moveCursor = () => {
+    if (favorites.value && favorites.value.length)
+        cursorObj.value.cursor = favorites.value[favorites.value.length - 1].id
+}
+
+watch(cursorObj.value, () => {
+    useLazyFetch(
+        `/api/user/${username.value}/favorites${
+            cursorObj.value.cursor ? `?cursor=${cursorObj.value.cursor}` : ""
+        }`,
+        {
+            key: `user/${username.value}/favorites${
+                cursorObj.value.cursor
+                    ? `?cursor=${cursorObj.value.cursor}`
+                    : ""
+            }`,
+        }
+    ).then((res) => {
+        const data = res.data?.value as {
+            favorites: PartialBy<FullPost, "author">[]
+            status: number
+        }
+        if (!data || !data.favorites || data.favorites?.length === 0) {
+            endOfFeed.value = true
+        } else if (data.favorites.length > 0) {
+            favorites.value = [...favorites.value, ...data.favorites]
+            favoritesStatus.value = data?.status || 0
+        }
+    })
+})
 </script>
 
 <template>
@@ -45,6 +85,10 @@ const reloadFavorites = () => {
             :favorites="post.favorites"
             :comments="post.comments"
             @reload-posts="reloadFavorites"
+        />
+        <InfiniteScroll
+            :end-of-feed="endOfFeed || (!pending && favorites.length < 10)"
+            @load-more-posts="moveCursor"
         />
     </section>
     <StatusComp v-else :status="favoritesStatus" />
