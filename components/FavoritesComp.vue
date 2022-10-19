37389ed1394abb8db842be7ce52dd87a8ad5110b
Tom Voet <tomvoet@gmx.de>
2022-10-19 10:49:46 +0200

    used new modalcomp

--- components/FavoritesComp.vue
+++ components/FavoritesComp.vue
@@ -53,17 +53,21 @@ const isLiked = (favorites: singleLike[] | undefined) =>
                 d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z"
             />
         </svg>
-        <ModalComp
-            :text="favorites ? String(favorites.length) : ''"
-            classes="ml-1"
-        >
-            <h4 class="text-xl font-bold">People who like this</h4>
-            <NuxtLink
-                v-for="favorite in favorites"
-                :key="favorite.username + 'fav'"
-                :to="'/user/' + favorite.username"
-                class="block p-1 hover:bg-gray-200"
-                >{{ favorite.username }}</NuxtLink
+        <ModalComp>
+            <template #button>
+                <span class="ml-1">{{
+                    favorites ? String(favorites.length) : ""
+                }}</span>
+            </template>
+            <template #modalContent>
+                <h4 class="text-xl font-bold">People who like this</h4>
+                <NuxtLink
+                    v-for="favorite in favorites"
+                    :key="favorite.username + 'fav'"
+                    :to="'/user/' + favorite.username"
+                    class="block p-1 hover:bg-gray-200"
+                    >{{ favorite.username }}</NuxtLink
+                ></template
             >
         </ModalComp>
     </div>
