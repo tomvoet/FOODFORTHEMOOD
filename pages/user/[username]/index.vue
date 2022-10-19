a69b8b1185420a22f6645b10c58fa7845b86417b
Tom Voet <tomvoet@gmx.de>
2022-10-19 19:14:07 +0200

    added infinite scroll to user posts page

--- pages/user/[username]/index.vue
+++ pages/user/[username]/index.vue
@@ -5,29 +5,65 @@ const route = useRoute()
 
 const username = computed(() => route.params.username as string)
 
-const posts = ref([] as PartialBy<FullPost, "author">[] | undefined | null)
+const cursorObj = ref({} as { cursor: number })
+const endOfFeed = ref(false)
+
+const posts = ref([] as PartialBy<FullPost, "author">[])
 const postsStatus = ref(0)
 
 const { data: postsData, pending } = useLazyFetch(
-    `/api/user/${username.value}/posts`,
+    `/api/user/${username.value}/posts${
+        cursorObj.value.cursor ? `?cursor=${cursorObj.value.cursor}` : ""
+    }`,
     {
-        key: `user/${username.value}/posts`,
+        key: `user/${username.value}/posts${
+            cursorObj.value.cursor ? `?cursor=${cursorObj.value.cursor}` : ""
+        }`,
     }
 )
 
-if (postsData) {
+if (postsData && postsData.value?.posts) {
     posts.value = postsData.value?.posts
     postsStatus.value = postsData.value?.status || 0
 }
 
 watch(postsData, (data) => {
-    posts.value = data?.posts
+    posts.value = data?.posts ? [...posts.value, ...data.posts] : []
     postsStatus.value = data?.status || 0
 })
 
 const reloadPosts = () => {
     refreshNuxtData()
 }
+
+const moveCursor = () => {
+    if (posts.value && posts.value.length)
+        cursorObj.value.cursor = posts.value[posts.value.length - 1].id
+}
+
+watch(cursorObj.value, () => {
+    useLazyFetch(
+        `/api/user/${username.value}/posts${
+            cursorObj.value.cursor ? `?cursor=${cursorObj.value.cursor}` : ""
+        }`,
+        {
+            key: `user/${username.value}/posts${
+                cursorObj.value.cursor
+                    ? `?cursor=${cursorObj.value.cursor}`
+                    : ""
+            }`,
+        }
+    ).then((res) => {
+        const data = res.data?.value as {
+            posts: PartialBy<FullPost, "author">[]
+            status: number
+        }
+        if (!data || !data.posts || data.posts?.length === 0)
+            endOfFeed.value = true
+        else posts.value = [...posts.value, ...data.posts]
+        postsStatus.value = data?.status || 0
+    })
+})
 </script>
 
 <template>
@@ -42,6 +78,10 @@ const reloadPosts = () => {
             :favorites="post.favorites"
             :comments="post.comments"
             @reload-posts="reloadPosts"
+        />
+        <InfiniteScroll
+            :end-of-feed="endOfFeed"
+            @load-more-posts="moveCursor"
         />
     </section>
     <StatusComp v-else :status="postsStatus" />
