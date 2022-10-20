dc27a0aee7c839d74c3b9f4536b8bf67a821b198
Tom Voet <tomvoet@gmx.de>
2022-10-20 21:09:24 +0200

    added imprint???

---
+++ pages/imprint.vue
@@ -0,0 +1,86 @@
+<template>
+    <article class="p-6">
+        <h2 class="text-4xl">Imprint</h2>
+        <h3 class="mt-3 text-2xl">Contact</h3>
+        <p>
+            <a href="mailto:foodforthemoodf4tm@gmail.com">
+                foodforthemoodf4tm@gmail.com
+            </a>
+        </p>
+        <h3 class="mt-3 text-2xl">Responsible for content</h3>
+        <p>Food4TheMood</p>
+        <h3 class="mt-3 text-2xl">Disclaimer</h3>
+        <p>
+            Food4TheMood is a project that was created for the purpose of
+            learning. It is not a commercial project and is not intended to make
+            any profit. All content provided on this website is for
+            informational purposes only. Food4TheMood makes no representations
+            as to the accuracy or completeness of any information on this site
+            or found by following any link on this site. Food4TheMood will not
+            be liable for any errors or omissions in this information nor for
+            the availability of this information. Food4TheMood will not be
+            liable for any losses, injuries, or damages from the display or use
+            of this information.
+        </p>
+        <p>
+            This site may contain links to other websites. These links are
+            provided for your convenience to provide further information. They
+            do not signify that Food4TheMood endorses the website(s).
+            Food4TheMood has no responsibility for the content of the linked
+            website(s).
+        </p>
+        <p>
+            Food4TheMood is not responsible for the content of external internet
+            sites. Links to external internet sites are provided as a
+            convenience and do not constitute an endorsement by Food4TheMood of
+            the content or views of the linked materials.
+        </p>
+        <p>
+            Food4TheMood is not responsible for the content of external internet
+            sites. Links to external internet sites are provided as a
+            convenience and do not constitute an endorsement by Food4TheMood of
+            the content or views of the linked materials.
+        </p>
+        <p>
+            The information contained in this website is for general information
+            purposes only. The information is provided by Food4TheMood and while
+            we endeavour to keep the information up to date and correct, we make
+            no representations or warranties of any kind, express or implied,
+            about the completeness, accuracy, reliability, suitability or
+            availability with respect to the website or the information,
+            products, services, or related graphics contained on the website for
+            any
+        </p>
+        <p>
+            purpose. Any reliance you place on such information is therefore
+            strictly at your own risk.
+        </p>
+        <p>
+            In no event will Food4TheMood be liable for any loss or damage
+            including without limitation, indirect or consequential loss or
+            damage, or any loss or damage whatsoever arising from loss of data
+            or profits arising out of, or in connection with, the use of this
+            website.
+        </p>
+        <p>
+            Through this website you are able to link to other websites which
+            are not under the control of Food4TheMood. We have no control over
+            the nature, content and availability of those sites. The inclusion
+            of any links does not necessarily imply a recommendation or endorse
+            the views expressed within them.
+        </p>
+        <p>
+            Every effort is made to keep the website up and running smoothly.
+            However, Food4TheMood takes no responsibility for, and will not be
+            liable for, the website being temporarily unavailable due to
+            technical issues beyond our control.
+        </p>
+        <h3 class="mt-3 text-2xl">Privacy Policy</h3>
+        <p>
+            Food4TheMood is committed to protecting your privacy. We will only
+            use the information that we collect about you lawfully (in
+            accordance with the Data Protection Act 1998 and the General Data
+            Protection Regulation (GDPR) 2018).
+        </p>
+    </article>
+</template>
