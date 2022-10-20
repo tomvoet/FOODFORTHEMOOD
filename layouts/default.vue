aee96bd7a45c415dd46677bdbaf986c9a02fdb08
Tom Voet <tomvoet@gmx.de>
2022-10-20 21:10:50 +0200

    improved showfab logic and adjusted to new font

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,23 @@
 <script lang="ts" setup>
+import { useUserStore } from "~~/stores/userStore"
+
 const titleChunk = useState("titleChunk")
 const descriptionRaw = useState("description")
+
+const route = useRoute()
+
+const userStore = useUserStore()
+
+const showFAB = computed(() => {
+    return (
+        route.path !== "/login" &&
+        route.path !== "/register" &&
+        route.path !== "/reset_password" &&
+        route.path !== "/settings" &&
+        route.path !== "/about" &&
+        userStore.loggedIn
+    )
+})
 
 const fullTitle = computed(() => {
     if (titleChunk.value) {
@@ -57,12 +74,12 @@ useHead({
         </main>
         <NuxtLoadingIndicator :height="5" />
 
-        <ModalComp>
+        <ModalComp v-if="showFAB">
             <template #button
                 ><div
                     class="fixed bottom-3 right-3 h-16 w-16 md:h-10 md:w-10 md:text-2xl bg-green-400 rounded-full flex items-center justify-center text-5xl text-white"
                 >
-                    +
+                    <span class="mb-1 md:mb-0">+</span>
                 </div></template
             ><template #modalContent><NewPostForm /></template
         ></ModalComp>
