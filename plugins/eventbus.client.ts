acf94f9ace901f3d17db8dd6c5b2117650746101
Tom Voet <tomvoet@gmx.de>
2022-10-22 00:23:24 +0200

    event bus plugin

---
+++ plugins/eventbus.client.ts
@@ -0,0 +1,12 @@
+import mitt from "mitt"
+const emitter = mitt()
+export default defineNuxtPlugin(() => {
+    return {
+        provide: {
+            bus: {
+                $on: emitter.on,
+                $emit: emitter.emit,
+            },
+        },
+    }
+})
