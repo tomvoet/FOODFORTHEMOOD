8654985a034045464e90677fefa2dbbfa1602192
Tom Voet <tomvoet@gmx.de>
2022-10-21 18:47:42 +0200

    Comments infinite scroll

--- components/CommentSection.vue
+++ components/CommentSection.vue
@@ -2,6 +2,10 @@
 import type { Comment } from "@prisma/client"
 import type { PartialBy } from "~~/customTypes"
 
+const route = useRoute()
+
+type ReducedComment = PartialBy<Comment, "postId">
+
 defineProps<{
     comments?: PartialBy<Comment, "postId">[]
     postId: number
@@ -14,6 +18,57 @@ const emits = defineEmits<{
 const reloadComments = () => {
     emits("reloadComments")
 }
+
+const cursorObj = ref({} as { cursor: number })
+const endOfFeed = ref(false)
+const comments = ref([] as ReducedComment[])
+const commentsStatus = ref(0)
+
+const { data: commentsData, error: commentsError } = getPostCommentsLazy(
+    Number(route.params.id),
+    cursorObj.value
+)
+
+if (!commentsError.value && commentsData && commentsData.value) {
+    const data = commentsData.value as ReducedComment[]
+    if (data.length === 0) {
+        endOfFeed.value = true
+    }
+    comments.value = data
+    commentsStatus.value = 200
+}
+
+watch(commentsData, (dataBad) => {
+    const data = dataBad as ReducedComment[]
+
+    if (data.length === 0) {
+        endOfFeed.value = true
+    }
+    comments.value = data ? [...comments.value, ...data] : []
+    commentsStatus.value = !commentsError.value ? 200 : 0
+})
+
+const moveCursor = () => {
+    cursorObj.value.cursor = comments.value[comments.value.length - 1].id
+}
+
+watch(cursorObj.value, () => {
+    getPostCommentsLazy(Number(route.params.id), cursorObj.value).then(
+        (res) => {
+            const data = res.data?.value as ReducedComment[]
+            console.log(data)
+            if (!data || data.length === 0) {
+                endOfFeed.value = true
+            } else {
+                comments.value = [...comments.value, ...data]
+                commentsStatus.value = !res.error.value ? 200 : 0
+                if (data.length < 10) {
+                    endOfFeed.value = true
+                }
+            }
+        }
+    )
+})
 </script>
 
 <template>
@@ -27,6 +82,11 @@ const reloadComments = () => {
                     @reload-comments="reloadComments"
                 />
             </div>
+            <InfiniteScroll
+                v-if="comments.length >= 10"
+                :end-of-feed="endOfFeed"
+                @load-more-posts="moveCursor"
+            />
         </template>
         <div v-else>No comments yet</div>
     </div>
