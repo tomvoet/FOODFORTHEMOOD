fd1b015bdd81576b55cf0962f988a40b4e889921
Tom Voet <tomvoet@gmx.de>
2022-10-22 00:48:06 +0200

    vitest fix?

--- vitest.config.ts
+++ vitest.config.ts
@@ -10,9 +10,41 @@ export default defineConfig({
         alias,
     },
     test: {
+        threads: false,
         environment: "jsdom",
-        deps: {
-            inline: [/@nuxt\/test-utils-edge/],
+        coverage: {
+            enabled: true,
+            lines: 90,
+            functions: 90,
+            branches: 90,
+            // We want to catch all js/ts/... files, not only the ones imported in some tests
+            // see https://github.com/bcoe/c8#checking-for-full-source-coverage-using---all
+            all: true,
+            include: [
+                // Nuxt 3 framework folders and files sources from directory structure here: https://v3.nuxtjs.org/guide/concepts/introduction
+                "assets",
+                "components",
+                "composables",
+                "layouts",
+                "middleware",
+                "pages",
+                "public",
+                "server",
+                "app.vue",
+            ],
+            exclude: [
+                "**/*.story.vue",
+                "**/*.test.ts",
+                "app.vue",
+                "pages/**/*.vue",
+                "server/database/index.ts",
+            ],
         },
+        deps: {
+            inline: [/@nuxt\/test-utils/],
+        },
+        globals: true,
+
+        setupFiles: ["./tests/testSetup.ts"],
     },
 })
