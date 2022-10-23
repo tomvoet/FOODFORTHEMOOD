8ba02b825f50d9f2c6d1cfa004ba9b61b0cd3382
Tom Voet <tomvoet@gmx.de>
2022-10-24 01:12:00 +0200

    added deleteCookie to test autoimports

--- vitest.config.ts
+++ vitest.config.ts
@@ -18,6 +18,7 @@ export default defineConfig({
                         "useBody",
                         "setCookie",
                         "parseCookies",
+                        "deleteCookie",
                     ],
                 },
             ],
