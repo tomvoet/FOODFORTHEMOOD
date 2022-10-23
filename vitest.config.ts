a6fe195fdea30a7934efe68cb9c5e17d78d8263d
Tom Voet <tomvoet@gmx.de>
2022-10-23 14:25:58 +0200

    testing config

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,52 +1,12 @@
-//
-//import { defineConfig } from "vite"
-//import { alias } from "./alias"
-//
-//export default defineConfig({
-//    root: ".",
-//    esbuild: {
-//        tsconfigRaw: "{}",
-//    },
-//    resolve: {
-//        alias,
-//    },
-//    test: {
-//        threads: false,
-//        environment: "jsdom",
-//        coverage: {
-//            enabled: true,
-//            lines: 90,
-//            functions: 90,
-//            branches: 90,
-//            // We want to catch all js/ts/... files, not only the ones imported in some tests
-//            // see https://github.com/bcoe/c8#checking-for-full-source-coverage-using---all
-//            all: true,
-//            include: [
-//                // Nuxt 3 framework folders and files sources from directory structure here: https://v3.nuxtjs.org/guide/concepts/introduction
-//                "assets",
-//                "components",
-//                "composables",
-//                "layouts",
-//                "middleware",
-//                "pages",
-//                "public",
-//                "server",
-//                "app.vue",
-//            ],
-//            exclude: [
-//                "**/*.story.vue",
-//                "**/*.test.ts",
-//                "app.vue",
-//                "pages/**/*.vue",
-//                "server/database/index.ts",
-//            ],
-//        },
-//        deps: {
-//            inline: [/@nuxt\/test-utils/],
-//        },
-//        globals: true,
-//
-//        setupFiles: ["./tests/testSetup.ts"],
-//    },
-//})
-//
+import { defineConfig } from "vite"
+import path from "path"
+
+export default defineConfig({
+    test: {
+        threads: false,
+        alias: {
+            "@": path.resolve(__dirname, "./src"),
+        },
+        setupFiles: ["./tests/setupTestUtils.ts"],
+    },
+})
