b98787cb4d987d53829c0851208d6854c66d3fa0
Tom Voet <tomvoet@gmx.de>
2022-10-24 01:00:52 +0200

    added aditional auto imports for tests

--- vitest.config.ts
+++ vitest.config.ts
@@ -17,6 +17,7 @@ export default defineConfig({
                         "createError",
                         "useBody",
                         "setCookie",
+                        "parseCookies",
                     ],
                 },
             ],
