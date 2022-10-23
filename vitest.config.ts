620fbbbce1ebdba08724c8bff7d5cc1272b1c6da
Tom Voet <tomvoet@gmx.de>
2022-10-23 14:26:17 +0200

    added auto imports to testing config

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,7 +1,25 @@
+import AutoImport from "unplugin-auto-import/vite"
+
 import { defineConfig } from "vite"
 import path from "path"
 
 export default defineConfig({
+    plugins: [
+        AutoImport({
+            include: [/\.vue$/, /\.vue\?vue/, /\.stories\.ts$/, /\.[tj]s$/],
+            imports: [
+                "vue",
+                {
+                    h3: [
+                        "defineEventHandler",
+                        "getQuery",
+                        "sendError",
+                        "createError",
+                    ],
+                },
+            ],
+        }),
+    ],
     test: {
         threads: false,
         alias: {
