20d23b6518351c8ba9309621ced3211728f21233
Tom Voet <tomvoet@gmx.de>
2022-10-24 22:51:37 +0200

    added additional data to restaurant

--- pages/restaurants/[id].vue
+++ pages/restaurants/[id].vue
@@ -1,9 +1,12 @@
 <script setup lang="ts">
-import type { simpleRestaurant } from "@/customTypes"
+import type { ExtendedRestaurant } from "@/customTypes"
 
 const route = useRoute()
 
-const restaurant = ref<simpleRestaurant>()
+const restaurant = useState<ExtendedRestaurant | null>(
+    "restaurant/" + route.params.id,
+    () => null
+)
 const restaurantStatus = useState("restaurantStatus", () => 0) //USESTATE DA SSR
 
 const { data, error } = await useFetch(`/api/restaurant/${route.params.id}`, {
@@ -46,18 +49,45 @@ setMetadata(
 
 <template>
     <div v-if="restaurantStatus == 200 && restaurant">
-        <section class="flex p-6 flex-row justify-between items-center">
-            <h2 class="text-3xl font-semibold">{{ restaurant?.name }}</h2>
-            <p v-if="restaurant.cuisines.length" class="text-center">
+        <section
+            class="flex p-6 flex-row justify-between items-center flex-wrap"
+        >
+            <h2 class="text-3xl font-semibold w-full sm:w-1/2 md:w-1/4">
+                {{ restaurant?.name }}
+            </h2>
+
+            <div class="w-full sm:w-1/2 md:w-1/4">
+                <StarRating :rating="stats?.rating || 0" />
+                <span>Reviews: {{ stats?.count || 0 }}</span>
+            </div>
+
+            <p
+                v-if="restaurant.cuisines.length"
+                class="text-left sm:text-center w-full sm:w-1/2 md:w-1/4"
+            >
                 &bull;
                 <template v-for="cuisine in restaurant.cuisines"
                     >{{ cuisine.name }} &bull;</template
                 >
             </p>
-            <p v-else>No Cuisines added yet</p>
-            <div>
-                <StarRating :rating="stats?.rating || 0" />
-                <span>Reviews: {{ stats?.count || 0 }}</span>
+            <p v-else class="w-full sm:w-1/2 md:w-1/4">No Cuisines added yet</p>
+
+            <div class="flex flex-col w-full sm:w-1/2 md:w-1/4">
+                <span>
+                    <span class="font-semibold">State:</span>
+                    {{ restaurant?.state }}
+                </span>
+                <span>
+                    <span class="font-semibold">City:</span>
+                    {{ restaurant?.city }}
+                </span>
+                <span>
+                    <span class="font-semibold">Address:</span>
+                    {{ restaurant?.street }} {{ restaurant?.houseNum }} <br />
+                    {{ restaurant?.zip }} {{ restaurant?.city }}
+                    <br />
+                    {{ restaurant?.state }}
+                </span>
             </div>
         </section>
         <NuxtPage />
