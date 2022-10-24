458f9948b0c53c8932c665d2314e1cdf1aae6684
Tom Voet <tomvoet@gmx.de>
2022-10-24 10:52:01 +0200

    more test cases

--- tests/api/auth.test.ts
+++ tests/api/auth.test.ts
@@ -3,6 +3,8 @@ import type { PathMethodHandler } from "../utils"
 import { setupApiAndDatabase } from "../utils"
 import loginHandler from "../../server/api/auth/login.post"
 import refreshTokenHandler from "../../server/api/auth/refresh_token"
+import resetPasswordHandler from "../../server/api/auth/reset_password.post"
+import logoutHandler from "../../server/api/auth/logout.post"
 
 const endpointBasePath = "/auth"
 const endpoints: PathMethodHandler[] = [
@@ -12,9 +14,19 @@ const endpoints: PathMethodHandler[] = [
         handler: loginHandler,
     },
     {
+        path: `${endpointBasePath}/logout`,
+        method: "post",
+        handler: logoutHandler,
+    },
+    {
         path: `${endpointBasePath}/refresh_token`,
         method: "get",
         handler: refreshTokenHandler,
+    },
+    {
+        path: `${endpointBasePath}/reset_password`,
+        method: "post",
+        handler: resetPasswordHandler,
     },
 ]
 
@@ -74,6 +86,40 @@ describe(`/auth`, async () => {
             })
         })
     })
+
+    describe("POST /logout", async () => {
+        describe("with valid refresh token", async () => {
+            const response = await request
+                .post(`${endpointBasePath}/logout`)
+                .set("Cookie", `refresh_token=${refresh_token}`)
+            it("should have status 200", async () => {
+                expect(response.statusCode).toBe(200)
+            })
+            it("should return a success message", async () => {
+                expect(response.body).toHaveProperty("statusMessage")
+                expect(response.body.statusMessage).toBe("Success")
+            })
+            it("should clear the refresh token cookie", async () => {
+                expect(response.headers["set-cookie"]).toBeDefined()
+                expect(response.headers["set-cookie"][0]).toMatch(
+                    /refresh_token/ && /Max-Age=0/
+                )
+            })
+        })
+        describe("with invalid refresh token", async () => {
+            const response = await request
+                .post(`${endpointBasePath}/logout`)
+                .set("Cookie", `refresh_token=invalidtoken`)
+            it("should have status 401", async () => {
+                expect(response.statusCode).toBe(401)
+            })
+            it("should return an error message", async () => {
+                expect(response.body).toHaveProperty("statusMessage")
+                expect(response.body.statusMessage).toBe("Unauthorized")
+            })
+        })
+    })
+
     describe("GET /refresh_token", async () => {
         describe("with valid refresh token", async () => {
             const response = await request
@@ -98,4 +144,31 @@ describe(`/auth`, async () => {
             })
         })
     })
+
+    describe("POST /reset_password", async () => {
+        describe("with valid email", async () => {
+            const response = await request
+                .post(`${endpointBasePath}/reset_password`)
+                .send({
+                    email: "foodforthemoodf4tm@gmail.com",
+                })
+
+            it("should have status 204", async () => {
+                expect(response.statusCode).toBe(204)
+            })
+        })
+        describe("with invalid email", async () => {
+            const response = await request
+                .post(`${endpointBasePath}/reset_password`)
+                .send({
+                    email: "invalidemail",
+                })
+
+            it("should have status 404", async () => {
+                expect(response.statusCode).toBe(404)
+            })
+        })
+    })
 })
+
+//todo register logout change password
