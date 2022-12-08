a7814481376d38c603c9d15785b1269514a15d3e
Tom Voet <tomvoet@gmx.de>
2022-12-08 10:35:48 +0100

    fixed minor starrating bug

--- components/StarRating.vue
+++ components/StarRating.vue
@@ -1,14 +1,18 @@
 <script setup lang="ts">
-defineProps<{
+const props = defineProps<{
     rating: number
 }>()
+
+const computedRating = computed(() => {
+    return props.rating
+})
 </script>
 
 <template>
     <div class="relative">
         <div
             class="absolute z-10 top-0 left-0 overflow-hidden text-yellow-400"
-            :style="{ width: `${rating * 20}%` }"
+            :style="{ width: `${computedRating * 20}%` }"
         >
             <span class="inline-block text-2xl">
                 &#9733;&#9733;&#9733;&#9733;&#9733;
